bcd0f3cda0a2cd3977597b136e5b3e87e248e5e6
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-18 05:27:39 +0530

    Create 05.cpp

---
+++ 05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ip;
+
+    //Getting input for integer variable ip.
+    cout<<"Enter value for ip : ";
+    cin>>ip;
+
+    //Output.
+    cout<<"ip : "<<ip<<endl;
+
+return 0;
+}
