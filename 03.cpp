561cde022f8c14ba3767cb569bdb05c99df22d55
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-17 19:53:48 +0530

    Update 03.cpp

--- 03.cpp
+++ 03.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
+    //Statements to print "Hello World!" in single line.
     cout<<"Hello ";
     cout<<"World!";
- return 0;
+
+    cout<<endl;
+
+    //Statement to print "Hello World!" in two line, using escape sequence.
+    cout<<"Hello \nWorld!";
+
+    cout<<endl;
+
+    //Statement to print "Hello World!" in two line, using manipulator.
+    cout<<"Hello "<<endl<<"World!";
+
+    return 0;
 }
