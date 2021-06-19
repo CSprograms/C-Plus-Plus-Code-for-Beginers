95a48d25ccd617b27d43935a22b4be0547b3dc1e
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-19 21:01:33 +0530

    Create 06.cpp

---
+++ 06.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ip1, ip2;
+    /*  int ip1;
+        int ip2;    */
+
+    //Getting input for integer variables ip1 and ip2.
+    cout<<"Enter input 1 (ip1) : ";
+    cin>>ip1;
+    cout<<"Enter input 2 (ip2) : ";
+    cin>>ip2;
+
+    //Output.
+    cout<<ip1<<" + "<<ip2<<" = "<<ip1+ip2<<endl;
+    cout<<ip1<<" - "<<ip2<<" = "<<ip1-ip2<<endl;
+    cout<<ip1<<" * "<<ip2<<" = "<<ip1*ip2<<endl;
+    cout<<ip1<<" / "<<ip2<<" = "<<ip1/ip2<<endl;
+    cout<<ip1<<" % "<<ip2<<" = "<<ip1%ip2<<endl;
+
+return 0;
+}
