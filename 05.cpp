9acaae0b3129548619781fd21d6ae88e06fd4382
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-21 12:58:35 +0530

    Update 05.cpp

--- 05.cpp
+++ 05.cpp
@@ -4,14 +4,23 @@ using namespace std;
 
 int main()
 {
-    int ip;
+    int ip1=10;     //Declaring and Initializing a variable.
 
-    //Getting input for integer variable ip.
-    cout<<"Enter value for ip : ";
-    cin>>ip;
+    int ip2{20};    //Value Initialization.
+    int ip3{};      //ip3 is initialized to default value, 0.
+
+    int ip4(40);    //Initializing a variable using parenthesis, Constructor type.
+    int ip5();      //ip5 is initialized to default value, 1.
+
+    int ip6;        //Unknown initial value.
 
     //Output.
-    cout<<"ip : "<<ip<<endl;
+    cout<<"ip1 : "<<ip1<<endl;
+    cout<<"ip2 : "<<ip2<<endl;
+    cout<<"ip3 : "<<ip3<<endl;
+    cout<<"ip4 : "<<ip4<<endl;
+    cout<<"ip5 : "<<ip5<<endl;
+    cout<<"ip6 : "<<ip6<<endl;
 
 return 0;
 }
