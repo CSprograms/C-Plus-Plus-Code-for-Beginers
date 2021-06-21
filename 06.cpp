12169b97cff1b6c4c5a3ac701c67d89344d80c7b
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-06-21 12:58:42 +0530

    Update 06.cpp

--- 06.cpp
+++ 06.cpp
@@ -4,26 +4,14 @@ using namespace std;
 
 int main()
 {
-    int ip1, ip2;
-    /*  int ip1;
-        int ip2;    */
+    int ip;
 
-    //Getting input for integer variables ip1 and ip2.
-    cout<<"Enter input 1 (ip1) : ";
-    cin>>ip1;
-    cout<<"Enter input 2 (ip2) : ";
-    cin>>ip2;
+    //Getting input for integer variable ip.
+    cout<<"Enter value for ip : ";
+    cin>>ip;
 
     //Output.
-    cout<<ip1<<" + "<<ip2<<" = "<<ip1+ip2<<endl;
-
-    cout<<ip1<<" - "<<ip2<<" = "<<ip1-ip2<<endl;
-
-    cout<<ip1<<" * "<<ip2<<" = "<<ip1*ip2<<endl;
-
-    cout<<ip1<<" / "<<ip2<<" = "<<ip1/ip2<<endl;
-
-    cout<<ip1<<" % "<<ip2<<" = "<<ip1%ip2<<endl;
+    cout<<"ip : "<<ip<<endl;
 
 return 0;
 }
