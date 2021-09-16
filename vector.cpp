eecb978f6ed35d1395bae352118f8e6796bfc72c
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 19:58:27 +0530

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,24 @@
+//
+// vector header
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> vec = {
+         "GCC", "Version", " : ", __VERSION__, "."
+    };
+    std::cout << vec << std::endl;
+}
